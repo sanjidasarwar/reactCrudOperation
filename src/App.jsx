994e36d051d77b1d9188f7afcfa92a4642c348b8
tupsi07 <sanjida657@yahoo.com>
2024-06-02 23:02:20 +0600

    json sever create

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Table from './components/Table'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [info, setIno] = useState([])
 
+  useEffect(()=>{
+    async function fetchData(){
+      const response = await fetch('http://localhost:3000/data')
+      const data =await response.json()
+      setIno(data)
+    }
+    fetchData()
+  }, [])
+  
+  console.log(info);
   return (
     <>
       <Table />
